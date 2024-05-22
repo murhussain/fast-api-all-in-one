971db2bb648b2593b1c46d31b18d14df65f9bbd0
Murhussain <husseinmurashi@yahoo.com>
2024-05-22 22:47:01 +0200

    Done with fastapi data interaction on GET and POST method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -6,3 +7,14 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Welcome to my first api!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is a list of posts"}
+
+
+@app.post("/post/create")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"New_post": f"Post created with title: {payload['title']} and body: {payload['content']}"}
