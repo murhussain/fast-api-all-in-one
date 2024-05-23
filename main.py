89364c13dca0d01fe11df3b0a8bb00d2da3a2144
Murhussain <husseinmurashi@yahoo.com>
2024-05-23 23:40:42 +0200

    Done with posting new post in the my_post list variable

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -15,12 +16,13 @@ class Post(BaseModel):
 
 
 my_post = [
-    {"title": "Post 1", "content": "This is the content of post 1", "published": True, "rating": 5},
-    {"title": "Post 2", "content": "This is the content of post 2", "published": False, "rating": 4},
-    {"title": "Post 3", "content": "This is the content of post 3", "published": True, "rating": 3},
-    {"title": "Post 4", "content": "This is the content of post 4", "published": False, "rating": 2},
-    {"title": "Post 5", "content": "This is the content of post 5", "published": True, "rating": 4},
+    {"id": 1, "title": "Post 1", "content": "This is the content of post 1", "published": True, "rating": 5},
+    {"id": 2, "title": "Post 2", "content": "This is the content of post 2", "published": False, "rating": 4},
+    {"id": 3, "title": "Post 3", "content": "This is the content of post 3", "published": True, "rating": 3},
+    {"id": 4, "title": "Post 4", "content": "This is the content of post 4", "published": False, "rating": 2},
+    {"id": 5, "title": "Post 5", "content": "This is the content of post 5", "published": True, "rating": 4},
 ]
+
 
 @app.get("/")
 async def root():
@@ -34,6 +36,10 @@ def get_posts():
 
 @app.post("/posts")
 def create_post(post: Post):
-    print(post)
-    print(post.dict())
-    return {"data": post}
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 1000000)
+    my_post.append(post_dict)
+    return {"data": {
+        "message": "Post created successfully",
+        "data": post_dict
+    }}
