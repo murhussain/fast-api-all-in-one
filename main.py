3b138ed92dbe4dbfdf6fbfd6e91ff023fdb8fd7b
Murhussain <husseinmurashi@yahoo.com>
2024-05-23 23:31:55 +0200

    Done with fetching posts list from the post variable list

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_post = [
+    {"title": "Post 1", "content": "This is the content of post 1", "published": True, "rating": 5},
+    {"title": "Post 2", "content": "This is the content of post 2", "published": False, "rating": 4},
+    {"title": "Post 3", "content": "This is the content of post 3", "published": True, "rating": 3},
+    {"title": "Post 4", "content": "This is the content of post 4", "published": False, "rating": 2},
+    {"title": "Post 5", "content": "This is the content of post 5", "published": True, "rating": 4},
+]
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to my first api!"}
@@ -21,10 +29,10 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is a list of posts"}
+    return {"data": my_post}
 
 
-@app.post("/post/create")
+@app.post("/posts")
 def create_post(post: Post):
     print(post)
     print(post.dict())
